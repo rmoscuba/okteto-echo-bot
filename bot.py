3beb36952fbe0b8ad8ed0769722f45be6fec9cfe
Rodolfo Ortega <34045496+rmoscuba@users.noreply.github.com>
2022-04-21 21:50:15 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,7 +51,7 @@ def echo(update: Update, context: CallbackContext) -> None:
 def main() -> None:
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
-    updater = Updater("TOKEN")
+    updater = Updater("5281522403:AAFJPXcEKtpXtvghxr_h5k3O3qUEcqb4_HQ")
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
